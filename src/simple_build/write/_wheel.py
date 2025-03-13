1cdb546e23ba06437820b438788361654ec59652
jui-wen.chen <jui-wen.chen@useblocks.com>
2025-03-13 14:39:27 +0100

    test

--- src/simple_build/write/_wheel.py
+++ src/simple_build/write/_wheel.py
@@ -56,8 +56,16 @@ def write_wheel(  # noqa: PLR0912
 
                 # TODO config options to include/exclude files in wheel modules,
                 # but they must allow for the behaviour above.
-
-                file_paths = gather_files(module, allow_non_git=True)
+                wheel_config = package.tool.get("wheel", {})
+                if not wheel_config:
+                    file_paths = gather_files(
+                        package.root,
+                        use_git=wheel_config.get("use_git", True),
+                        user_includes=wheel_config.get("include", []),
+                        user_excludes=wheel_config.get("exclude", []),
+                    )
+                else:
+                    file_paths = gather_files(module, allow_non_git=True)
             elif module.is_file():
                 file_paths = [module]
             else:
